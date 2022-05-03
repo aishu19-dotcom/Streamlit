ddc2a0d3cd97deb83b06f034982425673335672e
A P Aishwarya Lakshmi <77623254+aishu19-dotcom@users.noreply.github.com>
2022-05-03 19:42:58 +0530

    -

---
+++ Latex and Write.py
@@ -0,0 +1,17 @@
+#Latex: Is used to pass a sting or an simple expression 
+
+#How to use latex:
+st.latex(r''' //python code ''')
+
+#Write: 
+#This command performs different things depending upon the parameters you enter into it.
+#You can pass mulitple arguments and all will be printed.
+#It's behaviour depends on the input type
+
+#How to use write:
+st.write("Harsh","Guptha","python")
+st.write(st) #this shows the basic documentation of streamlit
+st.write(sum)
+st.write(d)
+st.write(data) #this can be used to display the data on the app (data frames)
+
